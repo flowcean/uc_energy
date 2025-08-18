c5bad68abdb90dbe2972d2dbbcb7225b6de55f61
Stephan Balduin <stephan.balduin@offis.de>
2025-08-18 14:32:20 +0200

    Fixed another error in run.py

--- run.py
+++ run.py
@@ -8,9 +8,8 @@ def main(steps):
 
 
 if __name__ == "__main__":
-    print(sys.argv)
     steps = 10
-    for i, arg in enumerate(sys.argv[1:]):
+    for i, arg in enumerate(sys.argv):
         if "steps" in arg and len(sys.argv) > i + 1:
             steps = int(sys.argv[i + 1])
 
